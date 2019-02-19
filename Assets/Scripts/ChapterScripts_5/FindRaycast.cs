5c911f7fe2f229ded573699e40d25a4a5a60f6f8
DayKui <15658052167@163.com>
2019-02-19 22:48:16 +0800

    引导ui聚合

---
+++ Assets/Scripts/ChapterScripts_5/FindRaycast.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+public class FindRaycast : MonoBehaviour
+{
+    /****在打开Raycast的ui上面显示蓝色线框提示*******/
+#if UNITY_EDITOR
+    static Vector3[] fourCorners = new Vector3[4];
+    void OnDrawGizmos()
+    {
+        foreach (MaskableGraphic g in GameObject.FindObjectsOfType<MaskableGraphic>())
+        {
+            if (g.raycastTarget)
+            {
+                RectTransform rectTransform = g.transform as RectTransform;
+                rectTransform.GetWorldCorners(fourCorners);
+                Gizmos.color = Color.blue;
+                for (int i = 0; i < 4; i++)
+                    Gizmos.DrawLine(fourCorners[i], fourCorners[(i + 1) % 4]);
+
+            }
+        }
+    }
+#endif
+}
+
