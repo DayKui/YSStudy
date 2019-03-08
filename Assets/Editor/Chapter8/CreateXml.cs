b1a7d302c7bc484a5686e1fce78bb72cc2800036
DayKui <15658052167@163.com>
2019-03-08 23:26:10 +0800

    增加xml和yaml解析

---
+++ Assets/Editor/Chapter8/CreateXml.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Xml;
+using System.IO;
+public class CreateXml {
+
+    [MenuItem("XML/WriteXML")]
+    static void WriteXml()
+    {
+        string xmlPath = Path.Combine(Application.dataPath,"test.xml");
+        if (File.Exists(xmlPath))
+        {
+            File.Delete(xmlPath);
+        }
+        //创建XmlDocument
+        XmlDocument xmlDoc = new XmlDocument();
+        XmlDeclaration xmlDeclation = xmlDoc.CreateXmlDeclaration("1.0","UTF-8",null);
+        xmlDoc.AppendChild(xmlDeclation);
+
+        //节点中写入数据
+        XmlElement root = xmlDoc.CreateElement("XmlRoot");
+        xmlDoc.AppendChild(root);
+
+        //循环写入三条数据
+        for (int i = 0; i < 3; i++)
+        {
+            XmlElement record = xmlDoc.CreateElement("Record");
+            record.SetAttribute("id",i.ToString());
+            record.SetAttribute("username","雨凇mono");
+            record.SetAttribute("password","123456");
+            root.AppendChild(record);
+        }
+        //写入文件
+        xmlDoc.Save(xmlPath);
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("XML/LoadXml")]
+    static void LoadXml()
+    {
+        string xmlPath = Path.Combine(Application.dataPath, "test.xml");
+        //xml文件只有存在才能读取
+        if (File.Exists(xmlPath))
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(xmlPath);
+            XmlNode nodes = xmlDoc.SelectSingleNode("XmlRoot");
+            foreach (XmlNode node in nodes.ChildNodes)
+            {
+                string id = node.Attributes["id"].Value;
+                string username = node.Attributes["username"].Value;
+                string password = node.Attributes["password"].Value;
+                Debug.LogFormat("id={0} username={1} password={2}", id, username, password);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ChapterScript_8/YamlStudy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YamlDotNet.RepresentationModel;
+using System.IO;
+public class YamlStudy : MonoBehaviour {
+
+    private IDictionary<YamlNode, YamlNode> m_MappingData;
+    private void Start()
+    {
+        //读取yaml字符串
+        string document = File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "yaml.txt"));
+        var input = new StringReader(document);
+        var yaml = new YamlStream();
+        yaml.Load(input);
+
+        //读取root点检节点
+        Debug.Log(yaml.Documents.Count+"==yamlCount");
+        var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+        m_MappingData = mapping.Children;
+    }
+    private void OnGUI()
+    {
+        GUILayout.Label(string.Format("<size=50>服务器列表:{0}</size>", m_MappingData["ServerList"]));
+        GUILayout.Label(string.Format("<size=50>服务器端口:{0}</size>", m_MappingData["Port"]));
+        GUILayout.Label(string.Format("<size=50>是否启动调试:{0}</size>", m_MappingData["Debug"]));
+    }
+
+}
