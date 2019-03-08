8eb4097ba278607c00d8a8d1f0e80fe735fd5445
DayKui <15658052167@163.com>
2019-03-09 00:33:58 +0800

    增加光照贴图的更换和预制体贴图信息的保存

---
+++ Assets/Scripts/ChapterScript_9/ChangeLightMap.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ChangeLightMap : MonoBehaviour
+{
+    //烘培贴图1
+    public Texture2D lightmap1;
+    //烘培贴图2
+    public Texture2D lightmap2;
+
+    public Texture2D lightMap3;
+    void OnGUI()
+    {
+        if (GUILayout.Button("<size=50>lightmap1</size>"))
+        {
+            LightmapData data = new LightmapData();
+            data.lightmapColor = lightmap1;
+
+            LightmapData data1 = new LightmapData();
+            data.lightmapColor = lightMap3;
+            LightmapSettings.lightmaps = new LightmapData[2] { data, data1 };
+        }
+
+        if (GUILayout.Button("<size=50>lightmap2</size>"))
+        {
+            LightmapData data = new LightmapData();
+            data.lightmapColor = lightmap2;
+
+            LightmapData data1 = new LightmapData();
+            data.lightmapColor = lightMap3;
+            LightmapSettings.lightmaps = new LightmapData[2] { data,data1 };
+        }
+    }
+}
---
+++ Assets/Scripts/ChapterScript_9/PrefabLightMap.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PrefabLightmap : MonoBehaviour
+{
+
+    public int lightmapIndex;
+    public Vector4 lightmapScaleOffset;
+
+    void Awake()
+    {
+        //Prefab实例化后赋值
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer)
+        {
+            renderer.lightmapIndex = lightmapIndex;
+            renderer.lightmapScaleOffset = lightmapScaleOffset;
+        }
+    }
+#if UNITY_EDITOR
+    [MenuItem("GameObject/Light/ToPrefab")]
+    static void ToPrefab()
+    {
+        //确保选择一个Hierarchy视图下的游戏对象
+        if (Selection.activeTransform)
+        {
+            Renderer renderer = Selection.activeTransform.GetComponent<Renderer>();
+            //确保有renderer组件
+            if (renderer)
+            {
+
+                PrefabLightmap prefabLightmap = Selection.activeTransform.GetComponent<PrefabLightmap>();
+                if (!prefabLightmap)
+                {
+                    prefabLightmap = Selection.activeTransform.gameObject.AddComponent<PrefabLightmap>();
+                }
+                prefabLightmap.lightmapIndex = renderer.lightmapIndex;
+                prefabLightmap.lightmapScaleOffset = renderer.lightmapScaleOffset;
+
+                Object prefab = PrefabUtility.GetPrefabParent(renderer.gameObject);//获取这个物体在project中的源prefab
+                                                                                   //如果有Prefab文件就更新，没有就创建新的                                                                  
+                if (prefab)
+                {
+                    PrefabUtility.ReplacePrefab(Selection.activeTransform.gameObject, prefab, ReplacePrefabOptions.ConnectToPrefab);
+                }
+                else
+                {
+                    PrefabUtility.CreatePrefab(string.Format("Assets/Resources/{0}.prefab", Selection.activeTransform.name), Selection.activeTransform.gameObject, ReplacePrefabOptions.ConnectToPrefab);
+                }
+            }
+        }
+    }
+#endif
+}
