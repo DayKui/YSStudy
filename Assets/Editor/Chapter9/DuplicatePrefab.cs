429b92c0264b51544e1e12b8d79b3ecf3630c00e
DayKui <15658052167@163.com>
2019-03-12 00:20:19 +0800

    增加动静批处理，和遮挡剔除

---
+++ Assets/Editor/Chapter9/DuplicatePrefab.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class DuplicatePrefab : MonoBehaviour
+{
+    [MenuItem("Tool/DuplicateGameObject %#d")]
+    static void DuplicateGameObject()
+    {
+        if (Selection.activeTransform)
+        {
+            Dictionary<string, Renderer> save = new Dictionary<string, Renderer>();
+
+            //根据相对路径保存Renderer信息
+            foreach (var renderer in Selection.activeTransform.GetComponentsInChildren<Renderer>())
+            {
+                string path = AnimationUtility.CalculateTransformPath(renderer.transform, Selection.activeTransform);
+                save[path] = renderer;
+            }
+            //执行复制
+            EditorApplication.ExecuteMenuItem("Edit/Duplicate");
+            //还原烘焙信息
+            foreach (var renderer in Selection.activeTransform.GetComponentsInChildren<Renderer>())
+            {
+                string path = AnimationUtility.CalculateTransformPath(renderer.transform, Selection.activeTransform);
+                if (save.ContainsKey(path))
+                {
+                    renderer.lightmapIndex = save[path].lightmapIndex;
+                    renderer.lightmapScaleOffset = save[path].lightmapScaleOffset;
+                }
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/ChapterScript_9/StaticBatch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticBatch : MonoBehaviour {
+
+    public GameObject[] datas;
+	// Use this for initialization
+	void Start () {
+        StaticBatchingUtility.Combine(datas,gameObject);
+	}
+	
+	
+}
