84a25728ee8c2eec62e5cd31b97abca36811f893
DayKui <15658052167@163.com>
2019-04-08 00:28:44 +0800

    增加ab和查找引用一级profiler导出

---
+++ Assets/Scripts/ChapterScript_11/CopyABToPersistent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class CopyABToPersistent : MonoBehaviour
+{
+
+    IEnumerator Start()
+    {
+
+        string path = Path.Combine(Application.streamingAssetsPath, "cube.unity3d");
+
+        //下载Bundle
+        WWW www = new WWW("file://" + path);
+        yield return null;
+        string name = Path.GetFileName(path);
+        string filePath = Path.Combine(Application.persistentDataPath, name);
+        if (File.Exists(filePath))
+        {
+            File.Delete(filePath);
+        }
+        //下载结束写入本地
+        File.WriteAllBytes(filePath, www.bytes);
+        Debug.Log(filePath);
+
+        //从硬盘中读取Bundle
+        AssetBundle assetbundle = AssetBundle.LoadFromFile(filePath);
+        //从Bundle中读取资源
+        GameObject prefab = assetbundle.LoadAsset<GameObject>("Cube");
+        //实例化资源
+        GameObject.Instantiate<GameObject>(prefab);
+    }
+}
---
+++ Assets/Scripts/ChapterScript_11/LoadProfiler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.Profiling;
+
+public class LoadProfiler : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("开始记录profiler"))
+        {
+            StartRecord();
+        }
+        if (GUILayout .Button("结束记录Profiler"))
+        {
+            StopRecord();
+        }
+    }
+
+    public static void StartRecord()
+    {
+        string fileName = string.Format("ProfilerData_{0}.data", DateTime.Now.ToString("hhmmss"));
+        if (Application.isEditor)
+        {
+            Profiler.logFile = Path.Combine(Application.dataPath+"/..",fileName);
+        }
+        else
+        {
+            Profiler.logFile = Path.Combine(Application.persistentDataPath,fileName);
+        }
+        Profiler.enabled = true;
+        Profiler.enableBinaryLog = true;
+    }
+    public static void StopRecord()
+    {
+        UnityEngine.Profiling.Profiler.enabled = false;
+        UnityEngine.Profiling.Profiler.enableBinaryLog = false;
+        UnityEngine.Profiling.Profiler.logFile = "";
+    }
+}
