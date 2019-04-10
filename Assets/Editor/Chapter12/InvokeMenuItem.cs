12ff920a55267aab62cf1a4cd124d1a9883d0e29
DayKui <15658052167@163.com>
2019-04-11 01:14:46 +0800

    增加内容

---
+++ Assets/Editor/Chapter12/InvokeMenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class InvokeMenuItem : AssetPostprocessor {
+
+    [MenuItem("Assets/AutoMenuItem",false,-1)]
+	static void AutoMenuItem()
+    {
+        Selection.activeObject = AssetDatabase.LoadAssetAtPath("Assets/Resources/Plane.prefab", typeof(Object));
+        //执行Command + D 复制
+        EditorApplication.ExecuteMenuItem("Edit/Duplicate");
+        //自动选择Prefab
+        Selection.activeObject = AssetDatabase.LoadAssetAtPath("Assets/Resources/Cube.prefab", typeof(Object));
+        //执行Command + D 复制
+        EditorApplication.ExecuteMenuItem("Edit/Duplicate");
+    }
+}
---
+++ Assets/Editor/Chapter12/SetDirty.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class SetDirty : MonoBehaviour {
+
+    [MenuItem("Assets/SetSceneDirty", false, -1)]
+    static void SetSceneDirty()
+    {
+        //设置场景dirty
+        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+        //设置Prefab dirty
+        EditorUtility.SetDirty(AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Cube.prefab"));
+    }
+}
